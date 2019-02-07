69ab7fe953f165c0c9ecd97ceab068c46e69cb81
Peter Huppertz <peter@dnl-core.net>
2019-02-07 12:24:16 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleDllProjectToTestTeamCity;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person
+            {
+                Name = "Peter",
+                BirthDate = new DateTime(1957, 12, 24)
+            };
+
+            Console.WriteLine($" I am now {me.AgeInDays} days old.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleDllProjectToTestTeamCity/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleDllProjectToTestTeamCity
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int AgeInDays
+        {
+            get { return (DateTime.Today - BirthDate).Days;  }
+        }
+    }
+}
